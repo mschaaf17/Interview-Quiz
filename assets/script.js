b369f38fc19045c9c3769efffffee788be937c81
Maddy <madisonschaaf48@gmail.com>
2022-02-09 13:48:56 -0700

    styling for home page and questions, added first question to alternate html

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//counter started for the first round
+var count = 60;
+
+//counter added if the answer is correct and subtract if wrong
+If(true) {
+count = count + 30;
+}
+else if (false) {
+count = count - 10;
+};
+
+
