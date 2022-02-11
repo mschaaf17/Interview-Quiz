f6d51f08da7954e7192f64041674ef008c018d5f
Maddy <madisonschaaf48@gmail.com>
2022-02-11 12:44:18 -0700

    changed question 1 html to scores

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,24 @@
-//when I click the start button a timer starts at 60 seconds
-var count = 60;
-
+var timerEl = document.getElementById('time-sec');
 //the time is shown in the header as a count down clock
-var countEl = document.querySelector('#count');
 //I click on an answer to the question
 //if the answer is correct, more time is added and I go to the next question
     //points should be added to my score everytime I get the answer correct
 //else if the is wrong, time is decreased and I go to the next question..loop?
-
-function setCounterText() {
-    countEl.textContent = count;
+function timer() {
+var timeLeft = 60;
+var countInterval = setInterval (function() {
+    if (timeLeft === 0) {
+        clearInterval(countInterval);
+        timerEl.textContent = '';
+    }
+    else if (timeLeft < 5) {
+        timerEl.textContent('.red') = timeLeft;
+        timeLeft--;
+    }
+    else {
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+    }
+}, 1000);
 }
-
-
-//when i run out of time or questions my score should appear on the final page
-
-
-
-//counter added if the answer is correct and subtract if wrong
-If(true) {
-count = count + 30;
-}
-else if (false) {
-count = count - 10;
-};
-
-
+timer();
