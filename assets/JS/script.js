38490ea23eff4d03dacc44dbc9eeb217ac269a99
Maddy <madisonschaaf48@gmail.com>
2022-02-13 19:12:51 -0700

    fixed button for answers

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,42 +1,40 @@
 var timerEl = document.getElementById('time-sec');
-var startBtn = document.getElementById("start-button");
-var scoreText = document.querySelector('#score');
-var directions = document.getElementById('directions');
 var questionEl = document.getElementById('question');
 var choices = Array.from(document.querySelectorAll('.choice-text'));
 const listEl = document.querySelector('.listEl');
 
 var currentQuestion= {}
 var acceptingAnswers = true
-var score = 0 
-var questionContainer = 0
 var availableQuestion = []
 
+var questions = [
+    {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choice1: 'JavaScript',
+    choice2: 'terminal/Bash',
+    choice3: 'for loops',
+    choice4: 'console.log',
+    answer: 2,
+    },
+    {
+        question: 'String values must be enclosed within ____ when being assigned to variables,',
+        choice1: 'commas',
+        choice2: 'curly brackets',
+        choice3: 'quotes',
+        choice4: 'parenthesis',
+        answer: 3,
+            },
+            {
+                question: 'JavaScript file has an extension of:',
+                choice1: '.Java',
+                choice2: '.Js',
+                choice3: '.javascript',
+                choice4: '.xml',
+                answer: 2,
+                    }
+                ]
 
-
-//var shuffledQuestions, currentQuestionIndex
-
-//start button 
-
-
-
-//displays instructions for quiz
-
-
-
-/*var listEl = document.createElement("ol");
-//answers for first question
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-//append the list
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
-*/
-
+const MAX_QUESTIONS = 3
 
 //starts the timer by clicking start button
 
@@ -64,7 +62,7 @@ var countInterval = setInterval (function() {
 }, 1000);
 }
 
-setNextQuestion = () => {
+function setNextQuestion() {
 if(availableQuestion.length === 0) {
     localStorage.setItem('mostRecentScore', timeLeft)
     return window.location.assign('./scores.html')
@@ -80,46 +78,37 @@ availableQuestion.splice(questionIndex, 1)
 acceptingAnswers = true
 }
 
-choices.forEach(choice => {
-    choice.addEventListener("click", event => {
+choiceOptions = Array.from(document.querySelectorAll('.choice-container')) 
+choiceOptions.forEach(choice => {
+    choice.addEventListener('click', e => {
+        
         if(!acceptingAnswers) return
+
         acceptingAnswers = false
-        const selectedChoice = event.target
+        let selectedChoice;
+        if(e.target.dataset.number) {
+            selectedChoice = e.target;
+        } else {
+            selectedChoice = e.target.children[1]
+        }
+        
         const selectedAnswer = selectedChoice.dataset['number']
+
+        var currentAnswer = currentQuestion.answer 
+        console.log(selectedAnswer, currentAnswer)
+
+        if(e.target = questions.answer) {
+            timerEl = timerEl + 10;
+        }
+        setNextQuestion();
     })
-    
-    });
+
+})
+
      
 
 //end
 //quiz questions
-var questions = [
-    {
-question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-choice1: 'JavaScript',
-choice2: 'terminal/Bash',
-choice3: 'for loops',
-choice4: 'console.log',
-answer: 2,
-    },
-    {
-        question: 'String values must be enclosed within ____ when being assigned to variables,',
-        choice1: 'commas',
-        choice2: 'curly brackets',
-        choice3: 'quotes',
-        choice4: 'parenthesis',
-        answer: 3,
-            },
-            {
-                question: 'JavaScript file has an extension of:',
-                choice1: '.Java',
-                choice2: '.Js',
-                choice3: '.javascript',
-                choice4: '.xml',
-                answer: 2,
-                    }
-                ]
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 3
+
 
 startGame()
