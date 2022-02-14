91118a48068ba5a58bf1de266c339f1503b7fc76
Maddy <madisonschaaf48@gmail.com>
2022-02-13 17:15:30 -0700

    made a seperate page for start button due to many conflicts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,27 @@
 var timerEl = document.getElementById('time-sec');
 var startBtn = document.getElementById("start-button");
-var results = document.getElementById('results');
+var scoreText = document.querySelector('#score');
 var directions = document.getElementById('directions');
-var questionContainerEl = document.getElementById('question-container')
 var questionEl = document.getElementById('question');
-const answerButtonsEl = document.getElementById('answer-buttons');
-var shuffledQuestions, currentQuestionIndex
+var choices = Array.from(document.querySelectorAll('.choice-text'));
+const listEl = document.querySelector('.listEl');
+
+var currentQuestion= {}
+var acceptingAnswers = true
+var score = 0 
+var questionContainer = 0
+var availableQuestion = []
+
+
+
+//var shuffledQuestions, currentQuestionIndex
 
 //start button 
 
 
 
 //displays instructions for quiz
-questionEl.textContent = 'Please click the button to continue;'
-directions.textContent = 'Read carefully';
-answerButtonsEl.setAttribute("style", "display: none");
+
 
 
 /*var listEl = document.createElement("ol");
@@ -32,17 +39,13 @@ listEl.appendChild(li4);
 
 
 //starts the timer by clicking start button
-startBtn.addEventListener("click", startGame)
-function startGame() {
-    console.log('started')
-    startBtn.setAttribute("style", "display: none");
-    shuffledQuestions = quizQues.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0;
-    directions.textContent ="";
-    answerButtonsEl.setAttribute("style", "display: visible")
-    timer();
-    setNextQuestion();
+
     
+function startGame() {
+    score = 0;
+    availableQuestion = [...questions]
+    setNextQuestion();
+    timer();
 }
 
 //timer function
@@ -52,7 +55,7 @@ var countInterval = setInterval (function() {
     if (timeLeft === 0) {
         clearInterval(countInterval);
         timerEl.textContent = 'Times out';
-        window.location.href="./question1.html";
+        window.location.href="./scores.html";
     }
     else {
         timerEl.textContent = timeLeft;
@@ -61,97 +64,62 @@ var countInterval = setInterval (function() {
 }, 1000);
 }
 
-//setting next questions
-function setNextQuestion() {
-resetState()
-showQuestion(shuffledQuestions[currentQuestionIndex])
+setNextQuestion = () => {
+if(availableQuestion.length === 0) {
+    localStorage.setItem('mostRecentScore', timeLeft)
+    return window.location.assign('./scores.html')
 }
-
-
-function showQuestion(question) {
-questionEl.innerText = question.question
-question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if(answer.correct) {
-        button.dataset.correct = answer.correct
-    }
-    button.addEventListener("click", selectAnswer)
-    answerButtonsEl.appendChild(button)
+const questionIndex = Math.floor(Math.random() * availableQuestion.length)
+currentQuestion = availableQuestion[questionIndex]
+question.innerText = currentQuestion.question
+choices.forEach(choice => {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
 })
+availableQuestion.splice(questionIndex, 1)
+acceptingAnswers = true
 }
 
-function resetState() {
-    //nextButton.classList.add('hide')
-    while(answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild
-        (answerButtonsEl.firstChild)
-    }
-}
-
-
-//selecting answer
-
-function selectAnswer(event) {
-const selectedButton = event.target;
-const correct = selectedButton.dataset.correct
-setStatusClass(document.body, correct)
-Array.from(answerButtonsEl.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-})
-if(shuffledQuestions.length > currentQuestionIndex + 1) {
-    window.location.href="./question1.html";
-}
-}
-
-function setStatusClass(element, correct) {
- //   clearStatusClass(element)
-    if(correct) {
-        timeLeft  = timeLeft + 10; 
-         timeLeft ++;
-    } 
-    else {
-        timeLeft = timeLeft - 10;
-            timeLeft--;
-    }
-    if (timeLeft === 0) {
-        window.location.href="./question1.html";
-    }
+choices.forEach(choice => {
+    choice.addEventListener("click", event => {
+        if(!acceptingAnswers) return
+        acceptingAnswers = false
+        const selectedChoice = event.target
+        const selectedAnswer = selectedChoice.dataset['number']
+    })
     
-}
+    });
+     
 
+//end
 //quiz questions
-const quizQues = [
+var questions = [
     {
-question: 'What is 2+2?',
-answers: [ 
-    {text: '4', correct: true},
-    {text: '22', correct: false}
-]
+question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+choice1: 'JavaScript',
+choice2: 'terminal/Bash',
+choice3: 'for loops',
+choice4: 'console.log',
+answer: 2,
     },
     {
-        question: 'String values must be enclosed within____ when being assigned to variable.',
-        answers: [
-            {text: 'commas', correct: false},
-            {text: 'curly brackets', correct: false},
-            {text: 'quotes', correct: true},
-            {text: 'parenthesis', correct: false}
-        ]
-    }
-    
-]
+        question: 'String values must be enclosed within ____ when being assigned to variables,',
+        choice1: 'commas',
+        choice2: 'curly brackets',
+        choice3: 'quotes',
+        choice4: 'parenthesis',
+        answer: 3,
+            },
+            {
+                question: 'JavaScript file has an extension of:',
+                choice1: '.Java',
+                choice2: '.Js',
+                choice3: '.javascript',
+                choice4: '.xml',
+                answer: 2,
+                    }
+                ]
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 3
 
-
-
-//info for high scores page
-var initalsInput = document.querySelector('#initals-input');
-var logScoreButton = document.querySelector('#log-score-btn')
-
-logScoreButton.addEventListenter("click", logScore) 
-    function logScore() {
-    console.log(logScoreButton);
-    var initals = initalsInput.value.trim();
-
-localStorage.setItem("UserInitials", JSON.stringify (initals));
-};
+startGame()
