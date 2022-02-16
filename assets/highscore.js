931ddf8d734efb597a2a0ca9e17cb5a020e35191
Maddy <madisonschaaf48@gmail.com>
2022-02-15 21:07:15 -0700

    added an list for scores

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,24 +1,33 @@
 var submitInitialsBtn = document.querySelector('#log-initials-btn')
 var userInput = document.querySelector('#initials-input')
 var finalScore = document.querySelector('#yourScore')
-var usersScore = document.querySelector('#usersScore')
+
+var highScores = JSON.parse(localStorage.getItem('highScores'))|| []
+var orderLi = document.createElement("ol");
+
+for(i=0; i < highScores.length; i++) {
+   var listElement = document.createElement("li");
+   listElement.textContent = highScores[i].userInitials + ": " + highScores[i].score; 
+   orderLi.appendChild(listElement);
+}
+document.body.appendChild(orderLi);
 
 finalScore.textContent = localStorage.getItem('yourScore', JSON.stringify (timeLeft));
 
 
 
-submitInitialsBtn.addEventListener("click", function(event) {
-    event.preventDefault()
+submitInitialsBtn.addEventListener("click", function() {
+    //event.preventDefault()
     var userStats = {
         userInitials: userInput.value.trim(),
         score: localStorage.getItem('yourScore', JSON.stringify (timeLeft))
     }
-    
+    highScores.push(userStats)
 
-localStorage.setItem('userInfo', JSON.stringify(userStats));
-usersScore.textContent = localStorage.getItem('userInfo', JSON.stringify(userStats));
+localStorage.setItem('highScores', JSON.stringify(highScores));
+
+
+
+
 });
-
-
-
 
